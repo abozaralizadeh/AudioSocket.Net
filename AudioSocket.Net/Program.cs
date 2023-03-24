2b46a1f5f52bc8435a6a7d04a65622ced1215766
Abozar <abozar.alizadeh-miab@vodafone.com>
2023-03-24 17:36:25 +0100

    init

---
+++ AudioSocket.Net/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+const int BUFFER_SIZE = 1_024;
+const byte KindHangup = 0x00;
+const byte KindID = 0x01;
+const byte KindSlin = 0x10;
+const byte KindError = 0xff;
+
+//var lengthbytes = new byte[] { 0x00, 0x10 };  
+//var x = ToDecimal(lengthbytes);
+
+while (true)
+{
+    try
+    {
+        await StartSocket();
+    }
+    catch { }
+}
+
+
+async Task StartSocket() {
+
+    // ToDo Delete
+    File.Delete("sampleoutputstream.slin");
+
+    //Dns.GetHostName()
+    //IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("localhost");
+
+
+    IPAddress ipAddress = new IPAddress(new byte[] { 0x7f, 0x00, 0x00, 0x01}); //ipHostInfo.AddressList[0];
+
+    IPEndPoint ipEndPoint = new(ipAddress, 5044);
+
+    using Socket listener = new(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+    listener.Bind(ipEndPoint);
+    listener.Listen(100);
+
+    string uuidString;
+    var handler = await listener.AcceptAsync();
+    var currentIndex = 0;
+    var remained = 0;
+    byte? lastType = null;
+    
+
+    //await handler.SendAsync(terminateBytes, 0);
+
+    try
+    {
+        while (true)
+        {
+            currentIndex = 0;
+            // Receive message.
+            var buffer = new byte[BUFFER_SIZE];
+            var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+            //var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+
+            if (buffer.Length > 0)
+            {
+
+                while (currentIndex < buffer.Length)
+                {
+                    if (remained == 0)
+                    {
+                        lastType = buffer[currentIndex];
+                    }
+
+                    if (lastType == KindHangup /* is end of message */)
+                    {
+                        Console.WriteLine(
+                            $"Socket server received message: 0x00");
+
+                        var echoBytes = new byte[] { 0x00, 0x00, 0x00 };
+                        //await handler.SendAsync(echoBytes, 0);
+                        Console.WriteLine(
+                            $"Socket server sent acknowledgment: 0x00,0x00, 0x00");
+
+                        break;
+                    }
+
+                    else if (lastType == KindID)
+                    {
+                        var length = ToDecimal(buffer.Take(new Range(1 + currentIndex, 3 + currentIndex)).ToArray());
+                        var UUID = buffer.Take(new Range(3 + currentIndex, (Index)(3 + currentIndex + length)));
+                        uuidString = ByteArrayToString(UUID.ToArray());
+                        currentIndex += (int)(3 + length);
+
+                        continue;
+                    }
+
+                    else if (lastType == KindError)
+                    {
+                        Console.WriteLine($"Socket error recieved");
+
+                        var length = ToDecimal(buffer.Take(new Range(1 + currentIndex, 3 + currentIndex)).ToArray());
+                        var errorCode = buffer.Take(new Range(3 + currentIndex, (Index)(3 + currentIndex + length)));
+                        var errorCodeString = ByteArrayToString(errorCode.ToArray());
+                        // ToDo handle the error
+                        currentIndex +=  (int)(3 + length);
+                    }
+
+                    else if (lastType == KindSlin)
+                    {
+                        Console.WriteLine("audio received");
+
+                        if (remained == 0)
+                        {
+                            var length = ToDecimal(buffer.Take(new Range(1 + currentIndex, 3 + currentIndex)).ToArray());
+                            remained = (int)length;
+                            currentIndex += 3;
+                        }
+
+                        byte[] payloadToStream;
+
+                        if (remained > BUFFER_SIZE - currentIndex)
+                        {
+                            payloadToStream = buffer.Take(new Range(currentIndex, BUFFER_SIZE)).ToArray();
+                            remained = remained - (BUFFER_SIZE - currentIndex);
+                            currentIndex = BUFFER_SIZE;
+
+                        }
+                        else
+                        {
+                            payloadToStream = buffer.Take(new Range(currentIndex, currentIndex + remained)).ToArray();
+                            currentIndex = currentIndex + remained;
+                            remained = 0;
+
+                        }
+                        // ToDo Stream the data to STT
+                        // CognitiveService.push(payloadToStream);
+                        try
+                        {
+                            var path = "sampleoutputstream.slin";
+
+                            var fileBytes = File.ReadAllBytes(path);
+                            File.WriteAllBytes("sampleoutputstream.slin", fileBytes.Concat(payloadToStream).ToArray());
+                        }
+                        catch {
+                            File.WriteAllBytes("sampleoutputstream.slin", payloadToStream);
+                        }
+                        // event arrived!
+                        // remained = 0;
+                        // currentIndex = 0;
+                        // var terminateBytes = new byte[] { 0x00, 0x00, 0x00 };
+                        // var x = terminateBytes.Take(new Range(0, 2));
+                        // break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(
+                            $"Type Unrecognised");
+                        break;
+                    }
+                }
+            }
+
+        }
+    } catch (Exception ex) {
+        Console.WriteLine(ex);
+        listener.Shutdown(SocketShutdown.Both);
+    }
+}
+
+static decimal ToDecimal(byte[] bytes)
+{
+    if (BitConverter.IsLittleEndian)
+        Array.Reverse<Byte>(bytes);
+    return BitConverter.ToUInt16(bytes);
+}
+
+static string ByteArrayToString(byte[] ba)
+{
+    return BitConverter.ToString(ba);
+}
---
+++ README.md
@@ -0,0 +1,85 @@
+# AudioSocket.Net
+
+A .Net sample implementation of Asterisk AudioSocket
+
+# AudioSocket
+
+[![](https://godoc.org/github.com/CyCoreSystems/audiosocket?status.svg)](http://godoc.org/github.com/CyCoreSystems/audiosocket)
+
+AudioSocket is a simple TCP-based protocol for sending and receiving realtime
+audio streams.
+
+There exists a protocol definition (below), a Go library, and Asterisk
+application and channel interfaces.
+
+**NOTE:**  as of 2020-01-15, AudioSocket has been included in the upstream Asterisk
+system.  While I am leaving the Asterisk patches here for use with previous
+versions, the Asterisk code in this repository should be considered obsolete.
+The Go code is up-to-date, maintained, and this is the primary source for it.
+
+## Protocol definition
+
+The singular design goal of AudioSocket is to present the simplest possible
+audio streaming protocol, initially based on the constraints of Asterisk audio.
+Each packet contains a three-byte header and a variable payload.  The header is
+composed of a one-byte type and a two-byte length indicator.
+
+The minimum message length is three bytes:  type and payload-length.  Hangup
+indication, for instance, is `0x00 0x00 0x00`.
+
+### Types
+
+  - `0x00` - Terminate the connection (socket closure is also sufficient)
+  - `0x01` - Payload will contain the UUID (16-byte binary representation) for the audio stream
+  - `0x10` - Payload is signed linear, 16-bit, 8kHz, mono PCM (little-endian)
+  - `0xff` - An error has occurred; payload is the (optional)
+    application-specific error code.  Asterisk-generated error codes are listed
+    below.
+
+### Payload length
+
+The payload length is a 16-bit unsigned integer (big endian) indicating how many bytes are
+in the payload.
+
+### Payload
+
+The content of the payload is defined by the header: type and length.
+
+### Asterisk error codes
+
+Error codes are application-specific.  The error codes for Asterisk are
+single-byte, bit-packed error codes:
+
+  - `0x01` - hangup of calling party
+  - `0x02` - frame forwarding error
+  - `0x04` - memory (allocation) error
+
+## Asterisk usage
+
+There are two Asterisk implementations: a channel interface and a dialplan
+application interface.  Each of these lends itself to simplify a different
+use-case, but they work in exactly the same way.
+
+The following examples demonstrate an AudioSocket connection to a server at
+`server.example.com` running on TCP port 9092.  The UUID (which is chosen
+arbitrarily) of the call is `40325ec2-5efd-4bd3-805f-53576e581d13`.
+
+Dialplan application:
+
+```
+exten = 100,1,Verbose("Call to AudioSocket via Dialplan Application")
+ same = n,Answer()
+ same = n,AudioSocket(40325ec2-5efd-4bd3-805f-53576e581d13,server.example.com:9092)
+ same = n,Hangup()
+```
+
+Channel interface:
+
+```
+exten = 101,1,Verbose("Call to AudioSocket via Channel interface")
+ same = n,Answer()
+ same = n,Dial(AudioSocket/server.example.com:9092/40325ec2-5efd-4bd3-805f-53576e581d13)
+ same = n,Hangup()
+```
+
+
