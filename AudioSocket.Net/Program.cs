221fb94a5144a2dbb9a447c5121f9110c849b6cb
Abozar <abozar.alizadeh@gmail.com>
2023-03-27 15:11:52 +0200

    Update Algorithm

--- AudioSocket.Net/Program.cs
+++ AudioSocket.Net/Program.cs
@@ -6,6 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 const int BUFFER_SIZE = 1_024;
+
+const int HEADER_BUFFER_SIZE = 3;
+
 const byte KindHangup = 0x00;
 const byte KindID = 0x01;
 const byte KindSlin = 0x10;
@@ -18,7 +21,7 @@ while (true)
 {
     try
     {
-        await StartSocket();
+        await ListenerSocket();
     }
     catch { }
 }
@@ -166,6 +169,126 @@ async Task StartSocket() {
 
         }
     } catch (Exception ex) {
+        Console.WriteLine(ex);
+        listener.Shutdown(SocketShutdown.Both);
+    }
+}
+
+
+async Task ListenerSocket()
+{
+
+    // ToDo Delete
+    File.Delete("sampleoutputstream.slin");
+
+    //Dns.GetHostName()
+    //IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("localhost");
+
+
+    IPAddress ipAddress = new IPAddress(new byte[] { 0x7f, 0x00, 0x00, 0x01 }); //ipHostInfo.AddressList[0];
+
+    IPEndPoint ipEndPoint = new(ipAddress, 5044);
+
+    using Socket listener = new(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+    listener.Bind(ipEndPoint);
+    listener.Listen(100);
+
+    string uuidString;
+    var handler = await listener.AcceptAsync();
+    byte? lastType = null;
+
+
+    //await handler.SendAsync(terminateBytes, 0);
+
+    try
+    {
+        while (true)
+        {
+            // Receive header.
+            var buffer = new byte[HEADER_BUFFER_SIZE];
+            var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+
+            var headerType = buffer[0];
+
+
+            if (headerType == KindHangup /* is end of message */)
+            {
+                Console.WriteLine(
+                    $"Socket server received message: 0x00");
+
+                var echoBytes = new byte[] { 0x00, 0x00, 0x00 };
+                //await handler.SendAsync(echoBytes, 0);
+                Console.WriteLine(
+                    $"Socket server sent acknowledgment: 0x00,0x00, 0x00");
+
+                continue;
+            }
+
+            else if (lastType == KindID)
+            {
+                var length = ToDecimal(buffer.Take(new Range(1, 3)).ToArray());
+
+                var bufferUUID = new byte[(int) length];
+                received = await handler.ReceiveAsync(bufferUUID, SocketFlags.None);
+                uuidString = ByteArrayToString(bufferUUID.ToArray());
+
+                continue;
+            }
+
+            else if (lastType == KindError)
+            {
+                Console.WriteLine($"Socket error recieved");
+
+                var length = ToDecimal(buffer.Take(new Range(1, 3)).ToArray());
+                var bufferError = new byte[(int)length];
+                received = await handler.ReceiveAsync(bufferError, SocketFlags.None);
+                var errorCodeString = ByteArrayToString(bufferError.ToArray());
+                // ToDo handle the error
+            }
+
+            else if (lastType == KindSlin)
+            {
+                Console.WriteLine("audio received");
+
+                var length = ToDecimal(buffer.Take(new Range(1 , 3)).ToArray());
+                var payloadToStream = new byte[(int)length];
+                received = await handler.ReceiveAsync(payloadToStream, SocketFlags.None);
+
+                // ToDo Stream the data to STT
+                // CognitiveService.push(payloadToStream);
+                try
+                {
+                    var path = "sampleoutputstream.slin";
+
+                    var fileBytes = File.ReadAllBytes(path);
+                    File.WriteAllBytes("sampleoutputstream.slin", fileBytes.Concat(payloadToStream).ToArray());
+                }
+                catch
+                {
+                    File.WriteAllBytes("sampleoutputstream.slin", payloadToStream);
+                }
+                // event arrived!
+                // remained = 0;
+                // currentIndex = 0;
+                // var terminateBytes = new byte[] { 0x00, 0x00, 0x00 };
+                // var x = terminateBytes.Take(new Range(0, 2));
+                // break;
+            }
+            else
+            {
+                Console.WriteLine(
+                    $"Type Unrecognised");
+                break;
+            }
+        }
+
+    }
+    catch (Exception ex)
+    {
         Console.WriteLine(ex);
         listener.Shutdown(SocketShutdown.Both);
     }
